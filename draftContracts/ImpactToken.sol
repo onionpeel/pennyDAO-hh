254c1f071fee5f8cff97339cc4cc2214e09e061e
Jeff Dinkelman <mfankenstein@yahoo.com>
2021-06-04 00:14:14 -0700

    begins Exchange.sol and ImpactToken.sol

--- draftContracts/ImpactToken.sol
+++ draftContracts/ImpactToken.sol
@@ -1,6 +1,8 @@
-// //SPDX-License-Identifier: MIT
-// pragma solidity 0.8.4;
-//
-// contract ImpactToken {
-//
-// }
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract ImpactToken is ERC20 {
+  constructor('Impact', 'IMPACT') {}
+}

2c6037db79d42d5783095486ab79d58176a3111a
Jeff Dinkelman <mfankenstein@yahoo.com>
2021-06-05 17:30:33 -0700

    updates Exchange.sol

--- Questions/developmentQuestions.js
+++ Questions/developmentQuestions.js
@@ -7,10 +7,12 @@ https://docs.openzeppelin.com/contracts/4.x/api/access
 
 4. Should public functions in ChangeMakers.sol be changed to external?
 
-5. How to handle IMPACT minting
-  a) initial amount?
-  b) mint as sponsors deposit?
-  c) burn as sponsors withdraw?
+// 5. How to handle IMPACT minting
+//   a) initial amount?
+//   b) mint as sponsors deposit?
+//   c) burn as sponsors withdraw?
 
 6. Should there be a function in ImpactToken, so that ChangeDao can mint IMPACT tokens and distribute those so that the recipient can they pick a project and use those tokens.
   Use case:  The community could vote to allocate funds from the community fund to a project directly.  Or instead of this, the community could vote to allocate IMPACT to groups/individuals and let them use those tokens as a way to choose which projects receive the community fund money.
+
+7. How to handle the keys of the ChangeDaoTeam and CommunityFund so that if that address needs to change, it can.
--- Questions/projectQuestions.js
+++ Questions/projectQuestions.js
@@ -4,11 +4,15 @@
     i) the changeMakers
     ii) the projects created by changeMakers
 
-2.  Flowchart change:
-  Sponsor does not actually send ERC20 tokens to the project.
-  --Need to figure out how this will work in the smart contracts, and then update flow Flowchart
+// 2.  Flowchart change:
+//   Sponsor does not actually send ERC20 tokens to the project.
+//   --Need to figure out how this will work in the smart contracts, and then update flow Flowchart
 
 3.  Flowchart change:
   When the loan is made whole, the Conversion contract interacts with Alchemix to get the stablecoin back.
 
 4. In what sense is ChangeDao a decentralized autonomous organization?  A lot of the control will still be held by the team behind ChangeDao.
+
+5. Changes in ChangeDao flow slide
+  a) remove erc20 token and replace with points mechanism in Exchange.sol
+  b) show direct funding from sponsor to a specific project

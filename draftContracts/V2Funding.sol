7bacbe5af5f7f863f632e9be6a391c585584cc80
Jeff Dinkelman <mfankenstein@yahoo.com>
2021-07-27 13:34:14 -0700

    v2funding

--- draftContracts/V2Funding.sol
+++ draftContracts/V2Funding.sol
@@ -3,35 +3,46 @@ pragma solidity 0.8.0;
 
 // This contract will be used to create clones
 contract V2Funding {
-  enum FundingSystem { UnSet, DirectFunding, Auction, CrowdFunding }
+  /// @notice Funding system options
+  enum FundingSystem { DirectFunding, Auction, CrowdFunding }
 
   FundingSystem public fundingSystem;
 
-  function initialize(uint _fundingSystem) {
-    fundingSystem = _fundingSystem;
+  /// @notice The project will set up the funding system based on the changeMaker's preference
+  /// @notice The type of funding system cannot be changed once it has been initialized
+  function initialize(uint _fundingSystem) public {
+    if (_fundingSystem == 0) {
+      fundingSystem = FundingSystem.DirectFunding;
+    } else if (_fundingSystem == 1) {
+      fundingSystem = FundingSystem.Auction;
+    } else {
+      fundingSystem = FundingSystem.CrowdFunding;
+    }
   }
 
+  /* @notice fund() routes calls to the appropriate funding implementation function based on the funding system that was set using initialize() */
   function fund() public {
-    if(fundingSystem == FundingSystem.DirectFunding) {
+    if (fundingSystem == FundingSystem.DirectFunding) {
       _directFund();
-    } else if {
-      if(fundingSystem == FundingSystem.Auction) {
-        _makeAuctionBid()();
+    } else if (fundingSystem == FundingSystem.Auction) {
+      _makeAuctionBid();
     } else {
       _crowdFund();
     }
   }
 
-
+  /// @notice A sponsor gives directly to the project
   function _directFund() private {
-    require(fundingSystem == 1, "Funding system must be DirectFunding");
+
   }
 
+  /// @notice Participants send in an auction bid
   function _makeAuctionBid() private {
-    require(fundingSystem == 2, "Funding system must be Auction");
+
   }
 
+  /// @notice A sponsor contributes toward a fundraising goal
   function _crowdFund() private {
-    require(fundingSystem == 3, "Funding system must be CrowdFunding");
+
   }
 }
